d790bfd1b129a5dd26ad0411f79de6ce4b643f0a
Mommy <mommy@Mommys-iMac.lan>
2021-12-26 14:47:06 -0600

    chore: try vagrant

--- starter/bootstrap.sh
+++ starter/bootstrap.sh
@@ -4,7 +4,7 @@ echo "[TASK 1] Install Docker"
 # install Docker
 zypper --non-interactive install docker
 systemctl enable docker
-usermod -G docker -a $USER
+usermod -a -G docker vagrant
 systemctl restart docker
 
 echo "[TASK 2] Disable firewalld"
@@ -21,10 +21,10 @@ usermod -a -G docker rke
 systemctl restart docker
 
 # echo "[TASK 4.5] Set up vagrant user"
-# cp -p /vagrant/.vagrant/machines/node1/virtualbox/private_key /home/vagrant/.ssh/id_rsa
+# cp -p /vagrant/.vagrant/machines/vanode1/virtualbox/private_key /home/vagrant/.ssh/id_rsa
 
 echo "[TASK 5] Copy auth_keys for rke user"
 mkdir -p /home/rke/.ssh
 usermod -d /home/rke/ rke
-cp /root/.ssh/authorized_keys /home/rke/.ssh
+cp /home/vagrant/.ssh/authorized_keys /home/rke/.ssh
 chown rke /home/rke -R
